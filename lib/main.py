0e10d755e2227a954a02296fc4d3def9b790b715
Kat <katrina@madetech.com>
2020-12-14 15:51:54 +0000

    Add tests

--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,14 @@
+from .gateways.here_to_help_api import HereToHelpGateway
 from .usecase.create_help_request import CreateHelpRequest
 from .lambda_handler import LambdaHandler
 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def lambda_handler(event, context):
-    create_help_request = CreateHelpRequest()
+    here_to_help_gateway = HereToHelpGateway()
+    create_help_request = CreateHelpRequest(gateway=here_to_help_gateway)
     handler = LambdaHandler(create_help_request)
+    print("event: ", event, "context:", context)
     return handler.execute(event, context)
-

65fa1dc2d18a7e8ff32f7ac4d08a23598c72d4f9
Kat <katrina@madetech.com>
2020-12-15 12:12:05 +0000

    Add create help request gateway method

--- lib/gateways/here_to_help_api.py
+++ lib/gateways/here_to_help_api.py
@@ -1,16 +1,33 @@
-# import os
-# import requests
+import os
+from urllib.error import HTTPError
+
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class HereToHelpGateway:
 
     def create_help_request(self, help_request):
-        # help_requests_url = os.getenv("CV-19-RES-SUPPORT-V3-HELP-REQUESTS-URL")
-        # headers = {
-        #     'Content-Type': 'application/json'
-        # }
-        #
-        # response = requests.request("POST", help_requests_url, headers=headers, data=help_request)
-        # print(response.text.encode('utf8'))
+        try:
+            help_requests_url = os.getenv("CV-19-RES-SUPPORT-V3-HELP-REQUESTS-URL")
+            headers = {
+                'Content-Type': 'application/json'
+            }
 
-        return help_request
+            response = requests.request("POST", help_requests_url, headers=headers, data=help_request)
+            print(response.text.encode('utf8'))
+        except HTTPError as err:
+            if err.code == 403:
+                print("Authentication error", err.msg)
+                return {}
+            else:
+                print("Could not create a new help request: ", help_request, err.msg)
+                return {}
+        except:
+            print("Could not create a new help request: ", help_request)
+            return {}
+
+        return json.loads(response.text)

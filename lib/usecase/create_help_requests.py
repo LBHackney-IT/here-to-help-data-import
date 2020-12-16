727ca81aa17057670bb7692cbb8c3dd842402837
Maysa <maysa@madetech.com>
2020-12-16 16:34:41 +0000

    pass gateway up from gateway to usecase

--- lib/usecase/create_help_requests.py
+++ lib/usecase/create_help_requests.py
@@ -9,14 +9,15 @@ class CreateHelpRequest:
         for help_request in help_requests:
             try:
                 response = self.gateway.create_help_request(help_request=help_request)
-                if response["Error"] is not None:
+                if "Error" in response:
+                    help_request['Error'] = response["Error"]
                     result["unsuccessful_help_requests"].append(help_request)
                 else:
                     result["created_help_request_ids"].append(response["id"])
             except Exception as err:
-                help_request["err"] = err
+                help_request['Error'] = str(err)
                 exceptions.append(help_request)
-                print("[CreateHelpRequestUseCase] Failed to create help request", err, help_request)
+                print("[CreateHelpRequestUseCase] Failed to create help request", str(err), help_request)
             if exceptions:
                 result["exceptions"] = exceptions
         return result

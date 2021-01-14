d47881b45cfd832101a82ce47d23982d672ef340
el <elena@madetech.com>
2021-01-14 12:38:31 +0000

    lint

--- lib_src/lib/gateways/gspread_drive_gateway.py
+++ lib_src/lib/gateways/gspread_drive_gateway.py
@@ -9,7 +9,8 @@ from gspread_formatting import *
 
 # Hopefully Kat has sorted the authentication part
 # You can make keys by going to https://console.cloud.google.com/iam-admin/
-# However that means you need access to the service account, which might only be for me [Huu Do]
+# However that means you need access to the service account, which might
+# only be for me [Huu Do]
 
 # key_file_location = 'Here2HelpKey.json'
 
@@ -21,40 +22,61 @@ class GSpreadGateway:
     OUTPUT = 'T&T daily outputs'
     SHEET_NAME = 'Sheet1'
     FIND_TERM = 'Category'
-    # have also included NHS Number, 'Date Updated', 'Date Time Extracted' as would be useful for data warehouse etc
-    COLS = ['Account ID', 'NHS Number', 'Forename', 'Surname', 'Gender', 'Date of Birth', 'House Number', 'Postcode',
-            'Email', 'Phone', 'Phone2', 'First Symptomatic At', 'Date Tested', 'Comments', 'Date Updated',
-            'Date Time Extracted']
-
+    # have also included NHS Number, 'Date Updated', 'Date Time Extracted' as
+    # would be useful for data warehouse etc
+    COLS = [
+        'Account ID',
+        'NHS Number',
+        'Forename',
+        'Surname',
+        'Gender',
+        'Date of Birth',
+        'House Number',
+        'Postcode',
+        'Email',
+        'Phone',
+        'Phone2',
+        'First Symptomatic At',
+        'Date Tested',
+        'Comments',
+        'Date Updated',
+        'Date Time Extracted']
 
     def __init__(self, key_file_location, google_drive_gateway):
 
         self.google_drive_gateway = google_drive_gateway
         scopes = ['https://spreadsheets.google.com/feeds']
 
-        creds = ServiceAccountCredentials.from_json_keyfile_name(key_file_location, scopes)
-        self.gspread_service = gspread.authorize(creds)  # makes the gspread service
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            key_file_location, scopes)
+        self.gspread_service = gspread.authorize(
+            creds)  # makes the gspread service
 
     def get_cases_from_gsheet(self, sheet_key):
-        print("[get_cases_from_gsheet] Trying to open by key using %s" % sheet_key)
+        print(
+            "[get_cases_from_gsheet] Trying to open by key using %s" %
+            sheet_key)
         spreadsheet = self.gspread_service.open_by_key(sheet_key)
         print(spreadsheet)
         sheet = spreadsheet.worksheet(self.SHEET_NAME)
-        # find first cell in T&T dowbnload - this should be 'Category', and use returned row value to get starting point of data
+        # find first cell in T&T dowbnload - this should be 'Category', and use
+        # returned row value to get starting point of data
         first_cell = sheet.find(self.FIND_TERM)
-        df = gspread_dataframe.get_as_dataframe(sheet, skiprows=2, parse_dates=True)
+        df = gspread_dataframe.get_as_dataframe(
+            sheet, skiprows=2, parse_dates=True)
         df = df.convert_dtypes()
         df = self.clean_data(df=df)
         print("[get_cases_from_gsheet] Displaying Dataframe")
         # print(df)
         return df  # returns dataframe
 
-    def clean_data(self, df): # takes whole sheet and lints data
+    def clean_data(self, df):  # takes whole sheet and lints data
         for i in self.COLS:
             df[i] = df[i].astype(str).str.strip().replace(r'\s+', '')
             df[i] = df[i].astype(str).str.strip().replace(r'nan', np.nan)
 
-        df['Account ID'] = df['Account ID'].astype(str) # some account ids are just numbers, we need them to be string
+        # some account ids are just numbers, we need them to be string
+        df['Account ID'] = df['Account ID'].astype(str)
         return df
 
     def create_output_spreadsheet(self, data_frame, outbound_folder_id):
@@ -63,11 +85,12 @@ class GSpreadGateway:
         today = datetime.today().strftime('%d/%m/%Y')
         # create new filename
         new_spreadsheet_name = f'Hackney_CT_FOR_UPLOAD_{today}'
-        # create a new spreadsheet TODO this only writes to personal drive so need way to write to shared drive - service account better for this?
+        # create a new spreadsheet TODO this only writes to personal drive so
+        # need way to write to shared drive - service account better for this?
 
         sh = self.gspread_service.open_by_key(
-            self.google_drive_gateway.create_spreadsheet(outbound_folder_id, new_spreadsheet_name)
-        )
+            self.google_drive_gateway.create_spreadsheet(
+                outbound_folder_id, new_spreadsheet_name))
         print(sh)
 
         # loop through each created dataframe and populate spreadsheet
@@ -85,10 +108,11 @@ class GSpreadGateway:
         today = datetime.today().strftime('%d/%m/%Y')
         # create new filename
         new_spreadsheet_name = f'city_CT_FOR_UPLOAD_{today}'
-        # create a new spreadsheet TODO this only writes to personal drive so need way to write to shared drive - service account better for this?
+        # create a new spreadsheet TODO this only writes to personal drive so
+        # need way to write to shared drive - service account better for this?
         sh = self.gspread_service.open_by_key(
-            self.google_drive_gateway.create_spreadsheet(outbound_folder_id, new_spreadsheet_name)
-        )
+            self.google_drive_gateway.create_spreadsheet(
+                outbound_folder_id, new_spreadsheet_name))
         # loop through each created dataframe and populate spreadsheet
         for i in data_frame:
             print(i[1])
@@ -103,9 +127,11 @@ class GSpreadGateway:
     def populate_output_sheets(self, spreadsheet, df, title):
         print("[populate_output_sheets]")
         # add a tab for each file and write each dataframe
-        worksheet = spreadsheet.add_worksheet(title=title, rows=df.shape[0] + 10, cols=len(self.COLS))
+        worksheet = spreadsheet.add_worksheet(
+            title=title, rows=df.shape[0] + 10, cols=len(self.COLS))
         next_row = self.next_available_row(worksheet)
-        gspread_dataframe.set_with_dataframe(worksheet, df, include_column_header=True, row=int(next_row), col=1)
+        gspread_dataframe.set_with_dataframe(
+            worksheet, df, include_column_header=True, row=int(next_row), col=1)
         # set_row_height(worksheet, f'1:{df.shape[0] + 10}', 21)
         return
 
--- lib_src/lib/usecase/find_and_process_new_sheet.py
+++ lib_src/lib/usecase/find_and_process_new_sheet.py
@@ -1,43 +1,54 @@
 import datetime as dt
+
+
 class FindAndProcessNewSheet:
 
     def __init__(self, google_drive_gateway, gspread_drive_gateway):
         self.google_drive_gateway = google_drive_gateway
         self.gspread_drive_gateway = gspread_drive_gateway
 
-    def execute(self, inbound_folder_id, outbound_folder_id ):
+    def execute(self, inbound_folder_id, outbound_folder_id):
         today = dt.datetime.now().date().strftime('%Y-%m-%d')
 
         print("[CheckLogic: %s] Begin Check" % dt.datetime.now())
 
-        if self.google_drive_gateway.searchFolder(inbound_folder_id, today, "spreadsheet") == True:
-            if self.google_drive_gateway.searchFolder(outbound_folder_id, today, "spreadsheet") == False:
-                print("[CheckLogic: %s] Run DataWrangleScript" % dt.datetime.now())
-                foundFileID = self.google_drive_gateway.getFile(inbound_folder_id, today, "spreadsheet")
+        if self.google_drive_gateway.searchFolder(
+                inbound_folder_id, today, "spreadsheet"):
+            if self.google_drive_gateway.searchFolder(
+                    outbound_folder_id, today, "spreadsheet") == False:
+                print(
+                    "[CheckLogic: %s] Run DataWrangleScript" %
+                    dt.datetime.now())
+                foundFileID = self.google_drive_gateway.get_file(
+                    inbound_folder_id, today, "spreadsheet")
                 print(foundFileID)
 
-                df = self.gspread_drive_gateway.get_cases_from_gsheet(foundFileID)
+                df = self.gspread_drive_gateway.get_cases_from_gsheet(
+                    foundFileID)
                 city_cases = self.gspread_drive_gateway.get_city_cases(df=df)
                 hackney_cases = self.get_hackney_cases(df=df)
                 text_list = self.get_text_message_list(df=hackney_cases)
-                address_list, email_list = self.get_address_email_lists(df=hackney_cases)
+                address_list, email_list = self.get_address_email_lists(
+                    df=hackney_cases)
                 city_dfs = [[city_cases, 'city_cases']]
-                output_dfs = [[email_list, 'email_list'], [address_list, 'address_list'],
-                              [hackney_cases, 'hackney_cases']]
-                self.gspread_drive_gateway.create_output_spreadsheet(data_frame=output_dfs, outbound_folder_id=outbound_folder_id)
-                self.gspread_drive_gateway.create_city_spreadsheet(data_frame=city_dfs, outbound_folder_id=outbound_folder_id)
+                output_dfs = [[email_list, 'email_list'], [
+                    address_list, 'address_list'], [hackney_cases, 'hackney_cases']]
+                self.gspread_drive_gateway.create_output_spreadsheet(
+                    data_frame=output_dfs, outbound_folder_id=outbound_folder_id)
+                self.gspread_drive_gateway.create_city_spreadsheet(
+                    data_frame=city_dfs, outbound_folder_id=outbound_folder_id)
                 cases_dict = self.hackney_cases_to_dict(df=hackney_cases)
                 print(cases_dict)
 
             else:
                 print(
-                    "[CheckLogic: %s] A file has been found in the output folder: https://drive.google.com/drive/folders/%s " % (
-                    dt.datetime.now(), outbound_folder_id))
+                    "[CheckLogic: %s] A file has been found in the output folder: https://drive.google.com/drive/folders/%s " %
+                    (dt.datetime.now(), outbound_folder_id))
                 print("[CheckLogic: %s] Will Abort")
         else:
             print(
-                "[CheckLogic: %s] No File found for todays PowerBI Output in folder: https://drive.google.com/drive/folders/%s " % (
-                dt.datetime.now(), inbound_folder_id))
+                "[CheckLogic: %s] No File found for todays PowerBI Output in folder: https://drive.google.com/drive/folders/%s " %
+                (dt.datetime.now(), inbound_folder_id))
             print("[CheckLogic: %s] Will Abort" % dt.datetime.now())
 
     # results = drive_service.files().list(supportsAllDrives=True, includeItemsFromAllDrives=True, q="parents in '{folder_id}' and trashed = false", fields = "nextPageToken, files(id, name)").execute()
@@ -53,26 +64,32 @@ class FindAndProcessNewSheet:
     def get_hackney_cases(self, df):
         print("[get_hackney_cases] Creating Hackney Case Dataframe")
         hack_df = df[df['UTLA'] == 'Hackney']
-        hack_df = hack_df[(~hack_df['Phone'].isna()) | (~hack_df['Phone2'].isna())]
+        hack_df = hack_df[(~hack_df['Phone'].isna()) |
+                          (~hack_df['Phone2'].isna())]
         # ensures there is at least one Phone Number
         hack_df = hack_df[self.gspread_drive_gateway.COLS]
         return hack_df
 
     def get_text_message_list(self, df):
         print("[get_text_message_list] Cases with no Phone Number for Text messaging")
-        text_list = df[~df['Phone'].isna()]  # Phone col appears to be always a mobile
+        # Phone col appears to be always a mobile
+        text_list = df[~df['Phone'].isna()]
         text_list = text_list[['Phone']]
         # print(text_list)
         return text_list
 
-    def get_address_email_lists(self, df):  # Only returns cases that don't have any phone number
-        print("[get_address_email_lists] Only returns cases that don't have any phone number")
-        address_list = df[
-            (df['House Number'].str.len() > 0) & (df['Postcode'].str.len() > 0) & (df['Phone'].str.len() == 0) & (
-                    df['Phone2'].str.len() == 0)]
-        address_list = address_list[['Date Time Extracted', 'Forename', 'Surname', 'House Number', 'Postcode']]
-        email_list = df[(df['Email'].str.len() > 0) & (df['Phone'].str.len() == 0) & (df['Phone2'].str.len() == 0)]
-        email_list = email_list[['Date Time Extracted', 'Forename', 'Surname', 'Email']]
+    # Only returns cases that don't have any phone number
+    def get_address_email_lists(self, df):
+        print(
+            "[get_address_email_lists] Only returns cases that don't have any phone number")
+        address_list = df[(df['House Number'].str.len() > 0) & (df['Postcode'].str.len(
+        ) > 0) & (df['Phone'].str.len() == 0) & (df['Phone2'].str.len() == 0)]
+        address_list = address_list[[
+            'Date Time Extracted', 'Forename', 'Surname', 'House Number', 'Postcode']]
+        email_list = df[(df['Email'].str.len() > 0) & (
+            df['Phone'].str.len() == 0) & (df['Phone2'].str.len() == 0)]
+        email_list = email_list[['Date Time Extracted',
+                                 'Forename', 'Surname', 'Email']]
         # print(address_list)
         # print(email_list)
         return address_list, email_list

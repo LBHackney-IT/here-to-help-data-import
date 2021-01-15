92b5ccc1cf4c0a80fd50b9b91995dddd26624220
el <elena@madetech.com>
2021-01-15 12:08:32 +0000

    print sys test

--- lib_src/lib/main.py
+++ lib_src/lib/main.py
@@ -1,37 +1,39 @@
 import json
 from dotenv import load_dotenv
-from .gateways.here_to_help_api import HereToHelpGateway
-from .gateways.gspread_drive_gateway import GSpreadGateway
-from .gateways.google_drive_gateway import GoogleDriveGateway
-from .usecase.create_help_requests import CreateHelpRequest
-from .usecase.find_and_process_new_sheet import FindAndProcessNewSheet
-from .lambda_handler import LambdaHandler
+# from .gateways.here_to_help_api import HereToHelpGateway
+# from .gateways.gspread_drive_gateway import GSpreadGateway
+# from .gateways.google_drive_gateway import GoogleDriveGateway
+# from .usecase.create_help_requests import CreateHelpRequest
+# from .usecase.find_and_process_new_sheet import FindAndProcessNewSheet
+# from .lambda_handler import LambdaHandler
+import sys
 
 load_dotenv()
 
 
 def lambda_handler(event, context):
-    here_to_help_gateway = HereToHelpGateway()
-    create_help_request = CreateHelpRequest(gateway=here_to_help_gateway)
-    handler = LambdaHandler(create_help_request)
+    print(sys.version)
+    # here_to_help_gateway = HereToHelpGateway()
+    # create_help_request = CreateHelpRequest(gateway=here_to_help_gateway)
+    # handler = LambdaHandler(create_help_request)
+    #
+    # print("------- Getting key_file_location")
+    #
+    # key_file_location = 'key_file.json'
+    # google_drive_gateway = GoogleDriveGateway(key_file_location)
+    # gspread_drive_gateway = GSpreadGateway(
+    #     key_file_location, google_drive_gateway)
+    #
+    # find_and_process_new_sheet = FindAndProcessNewSheet(
+    #     google_drive_gateway, gspread_drive_gateway)
+    #
+    # folders = json.loads(event)
+    #
+    # print("event: ", event, "context:", context)
+    #
+    # find_and_process_new_sheet.execute(
+    #     folders["inbound_folder_id"],
+    #     folders["outbound_folder_id"])
 
-    print("------- Getting key_file_location")
-
-    key_file_location = 'key_file.json'
-    google_drive_gateway = GoogleDriveGateway(key_file_location)
-    gspread_drive_gateway = GSpreadGateway(
-        key_file_location, google_drive_gateway)
-
-    find_and_process_new_sheet = FindAndProcessNewSheet(
-        google_drive_gateway, gspread_drive_gateway)
-
-    folders = json.loads(event)
-
-    print("event: ", event, "context:", context)
-
-    find_and_process_new_sheet.execute(
-        folders["inbound_folder_id"],
-        folders["outbound_folder_id"])
-
-    response = handler.execute(event, context)
-    return response
+    # response = handler.execute(event, context)
+    # return response

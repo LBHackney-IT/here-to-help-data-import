a9ca94e920d8920c85917b275f87819639ec8a60
el <elena@madetech.com>
2021-01-15 12:36:26 +0000

    exclude gspread

--- lib_src/lib/main.py
+++ lib_src/lib/main.py
@@ -1,26 +1,26 @@
 import json
 from dotenv import load_dotenv
-# from .gateways.here_to_help_api import HereToHelpGateway
+from .gateways.here_to_help_api import HereToHelpGateway
 # from .gateways.gspread_drive_gateway import GSpreadGateway
-# from .gateways.google_drive_gateway import GoogleDriveGateway
-# from .usecase.create_help_requests import CreateHelpRequest
+from .gateways.google_drive_gateway import GoogleDriveGateway
+from .usecase.create_help_requests import CreateHelpRequest
 # from .usecase.find_and_process_new_sheet import FindAndProcessNewSheet
-# from .lambda_handler import LambdaHandler
-import sys
+from .lambda_handler import LambdaHandler
 
 load_dotenv()
 
 
 def lambda_handler(event, context):
-    print(sys.version)
-    # here_to_help_gateway = HereToHelpGateway()
-    # create_help_request = CreateHelpRequest(gateway=here_to_help_gateway)
-    # handler = LambdaHandler(create_help_request)
-    #
-    # print("------- Getting key_file_location")
-    #
-    # key_file_location = 'key_file.json'
-    # google_drive_gateway = GoogleDriveGateway(key_file_location)
+    here_to_help_gateway = HereToHelpGateway()
+    create_help_request = CreateHelpRequest(gateway=here_to_help_gateway)
+    handler = LambdaHandler(create_help_request)
+
+    print("------- Getting key_file_location")
+
+    key_file_location = 'key_file.json'
+    google_drive_gateway = GoogleDriveGateway(key_file_location)
+
+    print(google_drive_gateway)
     # gspread_drive_gateway = GSpreadGateway(
     #     key_file_location, google_drive_gateway)
     #
@@ -35,5 +35,5 @@ def lambda_handler(event, context):
     #     folders["inbound_folder_id"],
     #     folders["outbound_folder_id"])
 
-    # response = handler.execute(event, context)
-    # return response
+    response = handler.execute(event, context)
+    return response

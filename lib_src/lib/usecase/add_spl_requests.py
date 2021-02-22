9dc84618897292c72772cc4b97340c2366988884
Ben <ben.dalton@madetech.com>
2021-02-22 15:20:48 +0000

    add spl request usecase

--- lib_src/lib/usecase/add_spl_requests.py
+++ lib_src/lib/usecase/add_spl_requests.py
@@ -3,19 +3,18 @@ from .helpers import parse_date_of_birth
 
 
 class AddSPLRequests:
-    def __init__(self, create_help_request, here_to_help_api):
+    def __init__(self, create_help_request):
         self.create_help_request = create_help_request
-        self.here_to_help_api = here_to_help_api
 
     def execute(self, data_frame):
         data_frame.insert(0, 'help_request_id', '')
 
         for index, row in data_frame.iterrows():
 
-            dob_day, dob_month, dob_year = parse_date_of_birth(row.date_of_birth)
+            dob_day, dob_month, dob_year = parse_date_of_birth(row.DateOfBirth, year_first=True)
 
             metadata = {
-                "nsss_id": row["ID"]
+                "spl_id": row.Traced_NHSNUMBER
             }
 
             author, note_date, nsss_case_note = self.get_case_note(row)
@@ -23,24 +22,24 @@ class AddSPLRequests:
             help_request = [
                 {
                     "Metadata": metadata,
-                    "Uprn": row.address_uprn,
-                    "Postcode": row.address_postcode.upper(),
-                    "AddressFirstLine": row.address_line1,
-                    "AddressSecondLine": row.address_line2,
-                    "AddressThirdLine": row.address_town_city,
+                    "Uprn": row.uprn,
+                    "Postcode": row.PatientAddress_PostCode.upper(),
+                    "AddressFirstLine": row.PatientAddress_Line1,
+                    "AddressSecondLine": row.PatientAddress_Line2,
+                    "AddressThirdLine": row.PatientAddress_Line3,
                     "CaseNotes": f'{{"author":"{author}","noteDate":" {note_date}","note":"{nsss_case_note}"}}',
                     "HelpWithSomethingElse": True,
-                    "FirstName": row.first_name.capitalize() if row.first_name else '',
-                    "LastName": row.last_name.capitalize() if row.last_name else '',
+                    "FirstName": row.PatientFirstName.capitalize() if row.PatientFirstName else '',
+                    "LastName": row.PatientSurname.capitalize() if row.PatientSurname else '',
                     "DobDay": f'{dob_day}',
                     "DobMonth": f'{dob_month}',
                     "DobYear": f'{dob_year}',
-                    "ContactTelephoneNumber": row.contact_number_calls,
-                    "ContactMobileNumber": row.contact_number_texts,
-                    "EmailAddress": row.contact_email,
-                    "CallbackRequired": self.is_called_required(row),
+                    "ContactTelephoneNumber": row.landline,
+                    "ContactMobileNumber": row.mobile,
+                    "EmailAddress": row.PatientEmailAddress,
+                    "CallbackRequired": False,
                     "HelpNeeded": "Shielding",
-                    "NhsNumber": row.nhs_number}]
+                    "NhsNumber": row.Traced_NHSNUMBER}]
 
             response = self.create_help_request.execute(
                 help_requests=help_request)
@@ -49,33 +48,12 @@ class AddSPLRequests:
                 help_request_id = response['created_help_request_ids'][0]
 
                 data_frame.at[index, 'help_request_id'] = help_request_id
-                request = self.here_to_help_api.get_help_request(
-                    help_request_id)
-
-                if not any(
-                        case_note['note'] == nsss_case_note for case_note in request['CaseNotes']):
-                    resident_id = request["ResidentId"]
-                    self.here_to_help_api.create_case_note(
-                        resident_id, help_request_id, {
-                            "author": author, "noteDate": note_date, "note": nsss_case_note})
 
         return data_frame
 
-    def is_called_required(self, row):
-        return True if row['do_you_need_someone_to_contact_you_about_local_support'].lower(
-        ) == 'yes' else False
-
     def get_case_note(self, row):
-        author = "Data Ingestion: National Shielding Service System list"
+        author = "Data Ingestion: Shielding Patient List"
         note_date = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S %Z")
-        case_note = 'CEV: Dec 2020 Tier 4 '
-        case_note += f'NSSS Submitted on:  ' + \
-            str(row['submission_datetime']) + '. '
-        case_note += 'Do you want supermarket deliveries? ' + \
-            row['do_you_want_supermarket_deliveries'] + '. '
-        case_note += 'Do you have someone to go shopping for you? ' + row[
-            'do_you_have_someone_to_go_shopping_for_you'] + '. '
-        case_note += 'Do you need someone to contact you about local support? ' + \
-            row['do_you_need_someone_to_contact_you_about_local_support'] + '.'
+        case_note = f'SPL Category: {row.category}'
 
         return author, note_date, case_note

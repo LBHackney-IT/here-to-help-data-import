ba04097bf766813eb745bae34ad0d28882f0d303
Kat <katrina@madetech.com>
2021-06-25 10:19:40 +0100

    Initial spike for adding self isolation cases throught HTH API

---
+++ lib_src/lib/usecase/process_self_isolation_calls.py
@@ -0,0 +1,32 @@
+from ..helpers import clean_data
+
+
+class ProcessSelfIsolationCalls:
+    COLS = [
+        'ID',
+        'NHS Number',
+        'Forename',
+        'Surname',
+        'Date of Birth',
+        'House Number',
+        'Address Line 1',
+        'Address Line 2',
+        'Town',
+        'Postcode',
+        'UPRN',
+        'Email',
+        'Phone',
+        'Phone2',
+        'LA Support Required',
+        'LA Support Letter Received'
+    ]
+
+    def __init__(self, add_self_isolation_requests):
+        self.add_self_isolation_requests = add_self_isolation_requests
+
+    def execute(self, data_frame):
+        data_frame = clean_data(columns=self.COLS, data_frame=data_frame)
+
+        processed_data_frame = self.add_self_isolation_requests.execute(data_frame)
+
+        return processed_data_frame
